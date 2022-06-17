7aa1ec9ea64ddc502fdd4b0cc82e7c31d3aca223
zzoo0123 <0123suh@gmail.com>
2022-06-18 07:11:42 +0900

    woochi move

---
+++ woochi/Assets/Script/woochiGame.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class woochiGame : MonoBehaviour
+{
+   public float movementSpeed = 3.0f;
+   Vector2 movement = new Vector2();
+   Rigidbody2D rigidbody2D;
+
+   Animator animator;
+   string animationsState = "woochiState";
+
+   enum States 
+   {
+	right = 1, 
+	left = 2, 	
+	up = 3, 
+	down = 4
+   }
+
+   void Start()
+   {
+	animator = GetComponent<Animator>();
+	rigidbody2D = GetComponent<Rigidbody2D>();
+   }
+
+   void Update() 
+   {
+   	UpdateState();
+   }
+
+   private void FixedUpdate()
+   {
+	MoveCharacter();
+   }
+
+   private void MoveCharacter()
+   {
+	movement.x = Input.GetAxisRaw("Horizontal");
+	movement.y = Input.GetAxisRaw("Vertical");
+
+	movement.Normalize();
+
+	rigidbody2D.velocity = movement * movementSpeed;
+    }
+
+    private void UpdateState() 
+    {
+	if (movement. x > 0)
+	    animator.SetInteger(animationsState, (int)States.right);
+	else if (movement.x < 0)
+	    animator.SetInteger(animationsState, (int)States.left);
+	else if (movement.y > 0)
+	    animator.SetInteger(animationsState, (int)States.up);
+	else if (movement.y < 0)
+	    animator.SetInteger(animationsState, (int)States.down);
+	else
+	    animator.SetInteger(animationsState, (int)States.down);
+    }
+
+}
